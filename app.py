7d7218081a1c6dd3b43c4f8f0b231fb715855849
pliegospro-maker <paqueteimpresiones@gmail.com>
2026-08-23 20:13:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ estilos = """
     # --- OCULTAR BARRA SUPERIOR DE STREAMLIT ---
 st.markdown(
     """
-    <style>
     /* Ocultar el Header completo (Toolbar y Menú Hamburguesa) */
     [data-testid="stHeader"] {
         display: none !important;
